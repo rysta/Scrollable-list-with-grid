6fdcf4573cb5ea2a86a55e1c79c449bfdf1d4b6b
rysta <rysta@mail.ru>
2023-06-28 15:14:10 +0300

    add topic card

--- app/src/main/java/com/example/scrollablelistwithgrid/MainActivity.kt
+++ app/src/main/java/com/example/scrollablelistwithgrid/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,9 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,8 +47,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TopicCard(topic: Topic, modifier: Modifier = Modifier){
-    Card(modifier = modifier.height(68.dp)){
-        Row(){
+    Card(
+        modifier = modifier
+            .height(68.dp)
+    ){
+        Row(
+            modifier = modifier
+        ){
             Image(
                 painter = painterResource(topic.imageResourceId),
                 contentDescription = stringResource(topic.nameResourceId),
@@ -58,36 +62,29 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier){
                     .width(68.dp),
                 contentScale = ContentScale.Crop
             )
-            Column(){
-                Text(stringResource(topic.nameResourceId))
-                Text(topic.courseId.toString())
-            }
+            TopicBody(topic, modifier)
         }
     }
+}
 
-//    Row(
-//        verticalAlignment = Alignment.CenterVertically,
-//        modifier = modifier
-//            .height(68.dp)
-//            .background(
-//                color = Color.LightGray,
-//                shape = RoundedCornerShape(10.dp)
-//            )
-//    ) {
-//        Image(
-//            painterResource(topic.imageResourceId),
-//            contentDescription = stringResource(topic.nameResourceId),
-//            modifier = modifier
-//                .fillMaxHeight()
-//                .width(68.dp)
-//                .clip(RoundedCornerShape(10.dp)),
-//            contentScale = ContentScale.Crop
-//        )
-//        Column {
-//            Text(stringResource(topic.nameResourceId))
-//            Text(topic.courseId.toString())
-//        }
-//    }
+@Composable
+fun TopicBody(topic: Topic, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)
+    ){
+        Text(text = stringResource(topic.nameResourceId), style = MaterialTheme.typography.bodyMedium)
+        Spacer(modifier = modifier.height(8.dp))
+        Row(
+            modifier = modifier
+        ){
+            Image(
+                painter = painterResource(R.drawable.ic_grain),
+                contentDescription = "grains"
+            )
+            Spacer(modifier = modifier.width(8.dp))
+            Text(text = topic.courseId.toString(), style = MaterialTheme.typography.labelMedium)
+        }
+    }
 }
 
 
@@ -95,6 +92,6 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier){
 @Composable
 fun CardPreview() {
     ScrollableListWithGridTheme {
-        TopicCard(Topic(R.string.architecture, 58, R.drawable.architecture))
+        TopicCard(Topic(R.string.architecture, 58, R.drawable.business))
     }
 }
