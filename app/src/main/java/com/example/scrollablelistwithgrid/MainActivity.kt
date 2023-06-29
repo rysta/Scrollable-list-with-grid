f5161bfe24bc4883bcdbabd91b68d811c6a7dfa9
rysta <rysta@mail.ru>
2023-06-29 10:07:06 +0300

    grid

--- app/src/main/java/com/example/scrollablelistwithgrid/MainActivity.kt
+++ app/src/main/java/com/example/scrollablelistwithgrid/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -12,19 +13,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.scrollablelistwithgrid.data.DataSource
 import com.example.scrollablelistwithgrid.model.Topic
 import com.example.scrollablelistwithgrid.ui.theme.ScrollableListWithGridTheme
 
@@ -38,9 +41,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TopicsGridApp(Modifier)
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TopicsGridApp(modifier: Modifier = Modifier){
+    TopicsGrid(
+        topics = DataSource().topics,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TopicsGrid(topics: List<Topic>, modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        modifier = modifier.padding(8.dp)
+    ){
+        items(topics){ topic ->
+            TopicCard(topic, modifier)
         }
     }
 }
@@ -92,6 +116,6 @@ fun TopicBody(topic: Topic, modifier: Modifier = Modifier){
 @Composable
 fun CardPreview() {
     ScrollableListWithGridTheme {
-        TopicCard(Topic(R.string.architecture, 58, R.drawable.business))
+        TopicsGridApp(Modifier)
     }
 }
